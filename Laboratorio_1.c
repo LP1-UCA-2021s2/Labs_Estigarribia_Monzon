85107e0cbfaa5ed865ead02e9d495f75f151eb14
Martin Monzon <74019803+c4russian@users.noreply.github.com>
2021-08-26 17:47:51 -0400

    Add files via upload

--- Laboratorio_1.c
+++ Laboratorio_1.c
@@ -11,22 +11,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define LENGTH 10
+//const int  LENGTH = 10;
 
 int main() {
-	int input[10],i=0;
+	int input[10];
+	for (int i=0;i < LENGTH;i++)
+		{
+			int valor;
+			printf("Ingrese los numeros enteros : ");
+			scanf("%d",&valor);
+			input[i]=abs(valor);
 
-	for (i < 10; i = i + 1 )
-	{
-		printf("Ingrese los numeros enteros : ");
+		}
 
-	}
+	order_ascending(input);
+
+	puts("La matriz ordenada: ");
+	for (int i = 0; i < LENGTH; i++)
+		{
+			if()
+			printf("%d ", input[i]);
+		}
+
+
+
+
+
 
 
 
 	return 0;
 }
 
-/*int verificacion()
+int order_ascending(int arr[])
 {
-
-}*/
+	int temp = 0;
+	for (int i = 0; i < LENGTH; i++)
+			{
+	        for (int j = i+1; j < LENGTH; j++)
+	        {
+	           if(arr[i] > arr[j]) {
+	               temp = arr[i];
+	               arr[i] = arr[j];
+	               arr[j] = temp;
+	           }
+	        }
+	    }
+	return 0;
+}
